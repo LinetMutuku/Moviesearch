2331135fe85268c1bba6b0397df063c41128e483
Linet  Mukai <151999722+LinetMutuku@users.noreply.github.com>
2024-06-13 13:38:18 +0300

    commits

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Searchmovies from './Searchmovies';
+
+
+export default function App(){
+    return (
+        <div className="container">
+         <h1 className='title'>Search Movies</h1>
+         <Searchmovies/>
+        </div>
+    )
+}
+
+
+  
---
+++ src/Searchmovies.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import MovieCard from './moviecard.js';
+import "./style.css"
+
+
+export default function SearchMovies(){
+    
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+    
+    const searchMovies = async (e) => {
+        e.preventDefault();
+                
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=352ad836ca4d4d24e0c5e0fb734cd942&language=en-US&query=${query}&page=1&include_adult=false`;
+        
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            setMovies(data.results);
+        }catch(err){
+            console.error(err);
+        }
+    }
+    
+    return (
+        <>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input className="input" type="text" name="query"
+                    placeholder="search here"
+                    value={query} onChange={(e) => setQuery(e.target.value)}
+                    />
+                    <br/>
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                   <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>    
+        </>
+    )
+}
